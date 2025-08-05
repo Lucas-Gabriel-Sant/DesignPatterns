9aaa793dc67c8af3b195a4ef8ff40dde1a89a214
User <User@DESKTOP-9MEA0MM.(none)>
2025-08-05 16:22:56 -0300

    Implementação do padrão Singleton

---
+++ Seção3/padrão_singleton.py
@@ -0,0 +1,17 @@
+
+
+class Singleton(object):
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+            print(f'Criando o objeto {Singleton}')
+        return cls.instance
+
+s1 = Singleton()
+print(f'Instância 1: {id(s1)}')
+
+s2 = Singleton()
+print(f'Instância 2: {id(s2)}')
+
+# duas intâncias que *deveriam* ser diferentes -> apenas um objeto
