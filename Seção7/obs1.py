faf0a993c2b10f1c6bb7c1f30db8063581042342
User <User@DESKTOP-9MEA0MM.(none)>
2025-10-09 20:01:03 -0300

    Aula prática do PP Observer

---
+++ Seção7/obs1.py
@@ -0,0 +1,43 @@
+
+
+class Objeto:
+
+    def __init__(self):
+        self.observadores = []
+
+    def __repr__(self):
+        return "::Objeto::"
+
+    def registrar(self, observador):
+        self.observadores.append(observador)
+
+    def notificar_todos(self, *args, **kwargs):
+        for observador in self.observadores:
+            observador.notificar(self, *args, **kwargs)
+
+
+class ObservadorA:
+
+    def __init__(self, objeto):
+        objeto.registrar(self)
+
+    def notificar(self, objeto, *args):
+        print(f'O {type(self).__name__} recebeu uma {args[0]} de {objeto}')
+
+
+class ObservadorB:
+
+    def __init__(self, objeto):
+        objeto.registrar(self)
+
+    def notificar(self, objeto, *args):
+        print(f'O {type(self).__name__} recebeu uma {args[0]} de {objeto}')
+
+
+
+obj = Objeto()
+
+obs_a = ObservadorA(obj)
+ob_b = ObservadorB(obj)
+
+obj.notificar_todos('notificação')
